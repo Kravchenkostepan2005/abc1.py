18cc67015244cd26eee8aeb0ae082a5e42ed9b77
Kravchenkostepan2005 <101333763+Kravchenkostepan2005@users.noreply.github.com>
2022-04-09 14:23:46 +0200

    abc1.py

---
+++ abc1.py
@@ -0,0 +1,40 @@
+class BankAccount:
+    def __init__(self, name):
+        self.list_bank = {}
+        self.name = name
+    def add_bank(self, bank_name, money):
+        self.list_bank[bank_name] = money
+    def getname(self):
+        return self.name
+    def info(self):
+        for key, val in self.list_bank.items():
+            print(f'bank name - {key}, balance - {val}')
+class ATM:
+    bank_name = "MonoBank"
+
+    def __init__(self, name, id, pin, balance=0):
+        self.__name = name
+        self.__id = id
+        self.__pin = pin
+        self.__balance = balance
+
+    def __print_private_account1_details(self):
+            return(self.__name, self.__id, self.__pin, self.__balance)
+
+    def check_private_details(self):
+        pin = input("Enter pin: ")
+        if self.__pin == int(pin):
+            return self.print_private_account1_details()
+        if self.__pin != int(pin):
+            return('Wrong pin!')
+
+    def change_pin_code(self):
+        pin = input("Pin: ")
+        if self.__pin == int(pin):
+            self.__pin == pin
+
+account_1 = ATM('User', 4698346, 6056, balance= 10000)
+
+print(account_1._ATM__print_private_account1_details())
+
+print(account_1._ATM__name)
